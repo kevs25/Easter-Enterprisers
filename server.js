660881e6f3b7cc0f08392c5cd90f9fe5aa6d7f43
kevs25 <kevindaniel200225@gmail.com>
2022-02-28 13:48:30 +0530

    server update

--- server.js
+++ server.js
@@ -132,6 +132,6 @@ app.get("/", (req, res) =>{
     res.sendFile(path.join(staticPath, "index.html"));
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('listening on port 3000......');
 })
